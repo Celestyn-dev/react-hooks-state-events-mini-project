5021ca351cabb3bd9fa7bbe0f35da7ddeb27de1c
Celestine Mecheo <celestinenyamoita094@gmail.com>
2025-04-20 22:27:23 +0300

    Complete task list app with working state and events

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("Code");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onTaskFormSubmit({ text, category });
+    setText("");
+    setCategory("Code");
+  }
+
+  const filteredCategories = categories.filter((cat) => cat !== "All");
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
+    <form className="new-task-form" onSubmit={handleSubmit}>
+      <label htmlFor="details">Details</label>
+      <input
+        id="details"
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <label htmlFor="category">Category</label>
+      <select
+        id="category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        {filteredCategories.map((cat) => (
+          <option key={cat}>{cat}</option>
+        ))}
+      </select>
+
       <input type="submit" value="Add task" />
     </form>
   );
